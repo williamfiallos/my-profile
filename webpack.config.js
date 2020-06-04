23507fa2abddb3506ec8b018ffad6b5f4de5bca8
williamfiallos <wfiallos10@gmail.com>
2020-06-04 16:40:09 -0700

    install babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         path: path.join(__dirname, "dist"), //absolute path, example: users/wfiallos/code/my_profile/<rootDir>/dist
         filename: 'main.js' //I can call it app.js if I want, or anything, and then npm run build again.
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //can also put .(js|jsx|ts)$
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets:["@babel/preset-env"]
+                }
+            }
+        ]
     }
 }
